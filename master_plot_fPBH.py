37ddaeaf3c3d15d0566b4de6d3b1780c9484c508
gortonm <73224026+gortonm@users.noreply.github.com>
2022-02-21 17:10:35 +0000

    Correct loading of number of events

--- master_plot_fPBH.py
+++ master_plot_fPBH.py
@@ -26,8 +26,8 @@ mpl.rcParams['ytick.minor.width'] = 1
 mpl.rcParams['xtick.direction'] = 'in'
 mpl.rcParams['ytick.direction'] = 'in'
 mpl.rcParams['lines.linewidth'] = 1.5
-mpl.rcParams['xtick.top'] = True
-mpl.rcParams['ytick.right'] = True
+mpl.rcParams['xtick.top'] = False
+mpl.rcParams['ytick.right'] = False
 mpl.rcParams['font.family'] = 'serif'
 mpl.rc('text', usetex=True)
 
@@ -41,14 +41,14 @@ def find_quantiles(x, minval=0.16, maxval=0.84):
 
 def error_bars(x):
     # calculate upper and lower 68% and 95% percentiles
-    x_68_lower, x_68_upper = find_quantiles(x)
+    x_68_lower, x_68_upper = find_quantiles(x) 
     x_95_lower, x_95_upper = find_quantiles(x, minval=0.025, maxval=0.975)
     
     # calculate arrays of 68% and 95% upper and lower error bars
     return [[np.mean(x) - x_68_lower], [x_68_upper - np.mean(x)]], [[np.mean(x) - x_95_lower], [x_95_upper - np.mean(x)]]
 
 
-def load_constraints():
+def load_constraints(Tisserand=True):
     """
     Load constraints on the PBH fraction from EROS-2, from Fig. 11 of Tisserand et al. (2007) (green curve)
 
@@ -61,7 +61,11 @@ def load_constraints():
         PBH mass m_pbh.
 
     """
-    file_constraints_CSV = open('./data_files/eros2_constraints_tisserand.csv')
+    if Tisserand:
+        name = 'tisserand'
+    else:
+        name = 'green_reproduction'
+    file_constraints_CSV = open('./data_files/eros2_constraints_' + name + '.csv')
     constraints_CSV = csv.reader(file_constraints_CSV)
     list_constraints_CSV = list(constraints_CSV)
         
@@ -76,54 +80,75 @@ def load_constraints():
 
 
 # Range of PBH masses to consider
-m_pbhs = 10**np.arange(1., 2.1, 0.5)
+m_pbhs = 10**np.arange(0., 1.51, 0.5)
 
 # Range of numbers of PBHs per cluster to consider
-n_cls = 10**np.arange(3, 7.1, 1.)
+n_cls = 10**np.arange(3, 8.1, 1.)
 
 # Number of realisations for each PBH mass and cluster size
 n_realisations = 1000
 
 # colours to plot for different cluster sizes
-colours = ['yellow', 'b', 'r', 'm']
+colours = ['darkturquoise', 'b', 'm', 'darkorange', 'r', 'saddlebrown']
 
 
 
-plt.figure()
-for j, n_cl in enumerate(n_cls):
-    
-    # displace positions of different cluster sizes on the x-axis
-    dx = 0.01 * (-len(n_cls)/2 + j)
-    
-    for m_pbh in m_pbhs:
-        f_pbhs = []    # create array of f_PBH values at fixed M_PBH
-        
-        for i in range(n_realisations):
-            # load file for number of events in a particular realisation
-            filepath = f'{os.getcwd()}' + '/simulated_data_constraints/N_cl/{0:.2f}'.format(np.log10(n_cl)) + '/M_PBH/{0:.2f}/'.format(np.log10(m_pbh))
-            n_ex_EROS_efficiency = np.loadtxt(filepath + 'n_ex_EROS.txt')
-            f_pbhs.append(3 / n_ex_EROS_efficiency)
-            
-        # calculate arrays of 68% and 95% upper and lower error bars
-        err_68, err_95 = error_bars(f_pbhs)
-        
-        # plot error bars and mean value
-        plt.errorbar(np.log10(m_pbh) + dx, np.mean(f_pbhs), yerr = err_68, linestyle='x', color=colours[j], elinewidth=1, capsize=2, label='$N_\mathrm{cl}' + ' = 10^{:.0f}'.format(np.log10(n_cl)))
-        plt.errorbar(np.log10(m_pbh) + dx, np.mean(f_pbhs), yerr = err_95, linestyle='x', color=colours[j], elinewidth=0.5, capsize=1)
-
+plt.figure(figsize=(8,4))
 
 """ Plot smooth constraint from EROS-2 (Fig. 15 of Tisserand et al. 2007 """
 m_pbh_smooth, f_pbh_smooth = load_constraints()
+plt.plot(m_pbh_smooth, f_pbh_smooth, color='lime')
+
+""" Plot smooth constraint from EROS-2 (Fig. 1 of Green 2016 """
+m_pbh_smooth, f_pbh_smooth = load_constraints(Tisserand=False)
 plt.plot(m_pbh_smooth, f_pbh_smooth, linestyle='dotted', color='k')
 
-plt.xlim(-2.5, 1.5)
-plt.ylim(1e-2, 1)
 
-plt.yscale('log')
-plt.legend()
+
+for j, n_cl in enumerate(n_cls):
+    
+    # displace positions of different cluster sizes on the x-axis
+    dx = 0.035 * (-len(n_cls)/2 + j*1.2)
+    
+    legend_list = []
+    
+    for m_pbh in m_pbhs:
+        
+        # load file for number of events in a particular realisation
+        filepath = f'{os.getcwd()}' + '/simulated_data_constraints/N_cl/{0:.2f}'.format(np.log10(n_cl)) + '/M_PBH/{0:.2f}/'.format(np.log10(m_pbh))
+        n_ex_EROS_efficiency = np.loadtxt(filepath + 'n_ex_EROS.txt')
+        
+        
+        f_pbhs = np.ones(len(n_ex_EROS_efficiency))
+        for i in range(len(n_ex_EROS_efficiency)):
+            if n_ex_EROS_efficiency[i] > 3:
+                f_pbhs[i] = (3 / n_ex_EROS_efficiency[i])
+        
+        # calculate arrays of 68% and 95% upper and lower error bars
+        err_68, err_95 = error_bars(f_pbhs)
+                
+        # plot error bars and mean value
+        plt.errorbar(np.log10(m_pbh) + dx, np.mean(f_pbhs), yerr = err_68, marker='x', color=colours[j], elinewidth=1, capsize=2)
+        plt.errorbar(np.log10(m_pbh) + dx, np.mean(f_pbhs), yerr = err_95, marker='x', color=colours[j], elinewidth=0.5, capsize=1)
+    
+        print(n_cl)
+        print(m_pbh)
+        print(err_68)
+        
+    point = plt.plot(np.log10(m_pbh) + dx, np.mean(f_pbhs), color = colours[j], marker='x', linestyle='none', label='$10^{:.0f}$'.format(np.log10(n_cl)))
+
+plt.legend(title='$N_\mathrm{cl}$')
+
+
+
+plt.xlim(-0.4, 2.1)
+plt.ylim(0., 1.01)
+
+#plt.yscale('log')
 plt.xlabel('$\log_{10}(M_\mathrm{PBH}/M_\odot)$')
 plt.ylabel('$f_\mathrm{PBH}$')
-plt.savefig('f_PBH_MPBH_noPoisson.pdf')
+plt.tight_layout()
+plt.savefig('f_PBH_MPBH.pdf')
 
             
             
@@ -132,6 +157,11 @@ plt.savefig('f_PBH_MPBH_noPoisson.pdf')
 sample = np.random.uniform(size=1000000)
 print(find_quantiles(sample))
 print(find_quantiles(sample, minval=0.025, maxval=0.975))
+
+
+err_68, err_95 = error_bars(sample)
+plt.errorbar(0., np.mean(sample), yerr = err_68, marker='x', color='r', elinewidth=1, capsize=2)
+plt.errorbar(0., np.mean(sample), yerr = err_95, marker='x', color='r', elinewidth=0.5, capsize=1)
 
 
 
